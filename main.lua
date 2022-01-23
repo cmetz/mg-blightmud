4dcf1280a5cd669b59abda8837a2cdcdfcd7fff6
Christoph Metz <loom@mopper.de>
2022-01-23 20:15:07 +0100

    script.reset() fix Fix the behavior when a script.reset() is called on an active connection

--- main.lua
+++ main.lua
@@ -3,7 +3,14 @@ local prompt = require("protocol.prompt")
 local mg_gmcp = require("protocol.gmcp")
 local statusbar = require("ui.statusbar")
 
+local self = {
+    host = store.session_read("cur_host"),
+    port = tonumber(store.session_read("cur_port") or "0")
+}
+
 local function on_connect(host, port)
+    store.session_write("cur_host", host)
+    store.session_write("cur_port", tostring(port))
     if host == "localhost" or host == "mg.mud.de" then
         statusbar:init()
         prompt:init()
@@ -12,6 +19,10 @@ local function on_connect(host, port)
 end
 
 local function on_disconnect()
+    self.host = nil
+    self.port = nil
+    store.session_write("cur_host", tostring(nil))
+    store.session_write("cur_port", tostring(nil))
     blight.status_height(1)
     blight.status_line(0, "")
     script.reset()
@@ -24,3 +35,7 @@ end)
 mud.on_disconnect(function()
     on_disconnect()
 end)
+
+if self.host and self.port then
+    on_connect(self.host, self.port)
+end
